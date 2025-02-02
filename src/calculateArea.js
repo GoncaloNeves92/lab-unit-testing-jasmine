3ef7102ecbfde5962c7489512d51124e79db4a71
Goncalo Neves <goncaloneves92@gmail.com>
2025-02-02 18:05:27 +0000

    Solved lab

--- src/calculateArea.js
+++ src/calculateArea.js
@@ -1,3 +1,9 @@
-function calculateArea() {
+function calculateArea(width , height) {
+    if (width == undefined || height == undefined) {
+        return undefined;
+    } if (typeof width != "number" || typeof height != "number") {
+        return undefined;
+    }
 
+    return width * height;
 }
